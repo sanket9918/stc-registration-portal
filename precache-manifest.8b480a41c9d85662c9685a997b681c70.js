80cbab900280372c4278c20863a2e396b4a9f53a
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2020-12-07 10:21:34 +0530

    Updates

---
+++ precache-manifest.8b480a41c9d85662c9685a997b681c70.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0329ca0d7c7cbaefd8624783a71f5e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57895cb295e6530630fe",
+    "url": "/static/css/main.4fd76544.chunk.css"
+  },
+  {
+    "revision": "73dc69f24e09d97dc33c",
+    "url": "/static/js/2.49292c41.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.49292c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57895cb295e6530630fe",
+    "url": "/static/js/main.368b1385.chunk.js"
+  },
+  {
+    "revision": "48bd4ecad8f058066cf5",
+    "url": "/static/js/runtime-main.d4b2682b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7a5d2dabe14e3d51a608e2b3c74b8dc1",
+    "url": "/static/media/global_error.7a5d2dab.svg"
+  },
+  {
+    "revision": "188f11caec6fa098d89e60a45c8d5db6",
+    "url": "/static/media/happy.188f11ca.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/static/media/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "5a2036234ef3104af7fd37e8003e9308",
+    "url": "/static/media/white.5a203623.svg"
+  }
+]);
